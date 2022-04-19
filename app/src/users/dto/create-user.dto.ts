9089320ba9229a4732a10da9e1b9c9846fe16e87
woorim960 <woorimprog@gmail.com>
2022-04-19 21:49:33 +0900

    Update: @transform -> @NotIn 커스텀 파이프로 변경

--- app/src/users/dto/create-user.dto.ts
+++ app/src/users/dto/create-user.dto.ts
@@ -1,5 +1,3 @@
-import { BadRequestException } from '@nestjs/common';
-import { Transform } from 'class-transformer';
 import {
   IsEmail,
   IsString,
@@ -7,15 +5,11 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { NotIn } from 'src/common/pipes/not-in.pipe';
 
 export class CreateUserDto {
-  @Transform(({ value, obj }) => {
-    if (obj.password.includes(value.trim())) {
-      throw new BadRequestException(
-        'password는 name과 같은 문자열을 포함할 수 없습니다.',
-      );
-    }
-    return value.trim();
+  @NotIn('password', {
+    message: 'password는 name과 같은 문자열을 포함할 수 없습니다.',
   })
   @IsString()
   @MinLength(2)
