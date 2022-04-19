b58355b66a7fd4fa3241d955a10cc8b6b99accb3
woorim960 <woorimprog@gmail.com>
2022-04-19 18:52:46 +0900

    Update: EmailModule 추가하여 EmailService를 독립적으로 분리

--- app/src/users/users.module.ts
+++ app/src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { EmailService } from 'src/email/email.service';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
+  imports: [EmailModule],
   controllers: [UsersController],
-  providers: [UsersService, EmailService],
+  providers: [UsersService],
 })
 export class UsersModule {}
