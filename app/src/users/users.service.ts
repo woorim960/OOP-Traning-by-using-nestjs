16196d499566ae4e0a5054d3108b380cc530d77c
woorim960 <woorimprog@gmail.com>
2022-04-19 16:56:07 +0900

    Update: createUser 메서드 기능구현을 위한 스켈레톤 코드로 변경

--- app/src/users/users.service.ts
+++ app/src/users/users.service.ts
@@ -1,18 +1,34 @@
+import * as uuid from 'uuid';
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UserLoginDto } from './dto/user-login.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExists(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  private checkUserExists(email: string) {
+    return false; // TODO: DB 연동 후 구현
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return; // TODO: DB 연동 후 구현
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    // await this.emailService.sendMemeberJoinVerification(
+    //   email,
+    //   signupVerifyToken,
+    // );
   }
 }
