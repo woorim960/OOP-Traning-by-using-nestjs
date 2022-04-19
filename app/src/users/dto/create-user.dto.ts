942442c5b392b9ac957ed13e8d59d0d9447be99e
woorim960 <woorimprog@gmail.com>
2022-04-19 21:31:15 +0900

    Add: CreateUserDto에 유효성 검사 파이프들 추가

--- app/src/users/dto/create-user.dto.ts
+++ app/src/users/dto/create-user.dto.ts
@@ -1,5 +1,33 @@
+import { BadRequestException } from '@nestjs/common';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(value.trim())) {
+      throw new BadRequestException(
+        'password는 name과 같은 문자열을 포함할 수 없습니다.',
+      );
+    }
+    return value.trim();
+  })
+  @IsString()
+  @MinLength(2)
+  @MaxLength(30)
   readonly name: string;
+
+  @IsString()
+  @IsEmail()
+  @MaxLength(60)
   readonly email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{8,30}$/)
   readonly password: string;
 }
