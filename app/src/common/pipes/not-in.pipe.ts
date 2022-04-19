5daa580f1ec32b6fd5d697c1412cb6a21d88050c
woorim960 <woorimprog@gmail.com>
2022-04-19 21:48:59 +0900

    Init: @NotIn 파이프 추가

---
+++ app/src/common/pipes/not-in.pipe.ts
@@ -0,0 +1,29 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator';
+
+export function NotIn(property: string, validationOptions?: ValidationOptions) {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return (object: Object, propertyName: string) => {
+    registerDecorator({
+      name: 'NotIn',
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: {
+        validate(value: any, args: ValidationArguments) {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as any)[relatedPropertyName];
+          return (
+            typeof value === 'string' &&
+            typeof relatedValue === 'string' &&
+            !relatedValue.includes(value)
+          );
+        },
+      },
+    });
+  };
+}
