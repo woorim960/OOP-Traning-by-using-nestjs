f5a26e155fe639a20fa4e02c387ba66bce0f6233
woorim960 <woorimprog@gmail.com>
2022-04-20 14:00:44 +0900

    Fix: export문 컴파일시 인식못해서 삭제 & __dirname을 dist로 변경

--- app/ormconfig.ts
+++ app/ormconfig.ts
@@ -1,10 +1,10 @@
-export default {
+module.exports = {
   type: 'mysql',
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+  entities: [`dist/**/*.entity{.ts,.js}`],
   synchronize: Boolean(process.env.DB_SYNC),
 };
