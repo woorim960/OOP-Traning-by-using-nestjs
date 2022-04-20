e99e11a567f1a22fdc4d1a9e419c2661466f1626
woorim960 <woorimprog@gmail.com>
2022-04-20 12:52:20 +0900

    Add: 유저 모듈에서 사용할 저장소 UserEntity 등록

--- app/src/users/entities/user.entity.ts
+++ app/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('User')
+export class UserEntity {
+  @PrimaryColumn()
+  id: string;
+
+  @Column({ length: 30 })
+  name: string;
+
+  @Column({ length: 60 })
+  email: string;
+
+  @Column({ length: 30 })
+  password: string;
+
+  @Column({ length: 60 })
+  signupVerifyToken: string;
+}
--- app/src/users/users.module.ts
+++ app/src/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { EmailModule } from 'src/email/email.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
-  imports: [EmailModule],
+  imports: [TypeOrmModule.forFeature([UserEntity]), EmailModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
