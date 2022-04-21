4fa8e35ef6b22f69e77747665c1da287457b1ce9
woorim960 <woorimprog@gmail.com>
2022-04-21 12:50:15 +0900

    Add: DB & JWT 환경변수도 검증하는 옵션 추가

--- app/src/config/env-validation-schema.ts
+++ app/src/config/env-validation-schema.ts
@@ -1,8 +1,19 @@
 import * as Joi from 'joi';
 
 export const envValidationSchema = Joi.object({
+  // EMAIL
   EMAIL_SERVICE: Joi.string().required(),
   EMAIL_AUTH_USER: Joi.string().required(),
   EMAIL_AUTH_PASSWORD: Joi.string().required(),
   EMAIL_BASE_URL: Joi.string().required().uri(),
+  // DB
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.string().required(),
+  DB_ROOT_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_SYNC: Joi.string().required(),
+  // JWT
+  JWT_SECRET: Joi.string().required(),
 });
