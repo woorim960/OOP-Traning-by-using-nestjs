2f87ed0a9f78c4d9a52932aaeb77ca6961099b5e
woorim960 <woorimprog@gmail.com>
2022-05-09 23:35:32 +0900

    Add: UserLoginDto의 유효성 검사 추가

--- app/src/users/dto/user-login.dto.ts
+++ app/src/users/dto/user-login.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, Matches } from 'class-validator';
+
 export class UserLoginDto {
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @Matches(/^[A-Za-z\d!@#$%^&*()]{9,30}$/)
   password: string;
 }
