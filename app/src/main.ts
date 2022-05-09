7fc6476fa6ea73db97f5505b4f696de1f8baa53b
woorim960 <woorimprog@gmail.com>
2022-05-09 23:31:55 +0900

    Add: DTO에 선언된 데이터의 요청만 허용하도록 ValidationPipe 옵션 추가

--- app/src/main.ts
+++ app/src/main.ts
@@ -11,7 +11,9 @@ async function bootstrap() {
   // pipes
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,
+      whitelist: true, // 엔티티 데코레이터에 없는 프로퍼티 값은 무조건 거름
+      forbidNonWhitelisted: true, // 엔티티 데코레이터에 없는 값 인입시 그 값에 대한 에러메세지 알려줌
+      transform: true, // 컨트롤러가 값을 받을때 컨트롤러에 정의한 타입으로 형변환
     }),
   );
 
