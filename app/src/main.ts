d8ed079834cebd624eba078fda544dfbcd342dad
woorim960 <woorimprog@gmail.com>
2022-05-24 02:15:13 +0900

    Add: logger 옵션 등록

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,10 +1,30 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 import { AppModule } from './app.module';
 import { logger2 } from './logger/logger2.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike('OOP_TRANING', {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  });
+
   // middlewares
   app.use(logger2);
 
